1021147d475d5a2c8944e63212c3677765a529bc
stnqls <stnqls3938@gmail.com>
2021-06-29 01:42:11 +0900

    조건문, 반복문

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,24 @@
   <h1 @click="increase">
     {{ count }}
   </h1>
+  <div v-if="count > 4">4보다 큽니다!</div>
+  <ul>
+    <Fruit v-for="fruit in fruits" :key="fruit" :name="fruit">
+      {{ fruit }}
+    </Fruit>
+  </ul>
 </template>
 
 <script>
+import Fruit from "~/components/Fruit";
 export default {
+  components: {
+    Fruit,
+  },
   data() {
     return {
       count: 0,
+      fruits: ["Apple", "Banana", "Cherry"],
     };
   },
   methods: {
@@ -19,9 +30,14 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 h1 {
   font-size: 50px;
   color: royalblue;
+}
+ul {
+  li {
+    font-size: 40px;
+  }
 }
 </style>
